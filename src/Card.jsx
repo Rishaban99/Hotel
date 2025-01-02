dfad6081279361bbb4871140a5c9a186d6211a0f
Rishaban99 <rishaban6012@gmail.com>
2025-01-02 11:27:04 +0530

    order_button add

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,11 @@
 function Card(props) {
   return (
     <div className="card">
-      <center>
-        <img className="card_profile" src={props.link}></img>
-      </center>
+      <img className="card_profile" src={props.link}></img>
 
       <h1 className="card_tittle">{props.name}</h1>
       <p className="card_peragrap">price : {props.price}.00</p>
+      <button className="Order_button">Order</button>
     </div>
   );
 }
